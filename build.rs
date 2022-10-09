c9e14cdd6e0d6b4b43e66964ab88b0b0501dda12
Daniel Bittman <danielbittman1@gmail.com>
2022-10-09 06:20:23 +0000

    Fix bad cfg error.

--- build.rs
+++ build.rs
@@ -27,7 +27,7 @@ const CHECK_CFG_EXTRA: &'static [(&'static str, &'static [&'static str])] = &[
     (
         "target_os",
         &[
-            "switch", "aix", "ohos", "hurd", "rtems", "visionos", "nuttx", "cygwin",
+            "switch", "aix", "ohos", "hurd", "rtems", "visionos", "nuttx", "cygwin", "twizzler"
         ],
     ),
     ("target_env", &["illumos", "wasi", "aix", "ohos"]),
