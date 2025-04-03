f469e6301bfbd22d5743d9c4b972b3ff3e9e3e5f
Daniel Bittman <danielbittman1@gmail.com>
2025-04-02 20:25:53 -0700

    Update toolchain.

--- src/twizzler/mod.rs
+++ src/twizzler/mod.rs
@@ -1,30 +1,11 @@
 // libc port for Twizzler
 
-cfg_if! {
-    if #[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))] {
-        pub type c_char = u8;
-    } else {
-        pub type c_char = i8;
-    }
-}
+use crate::prelude::*;
 
-pub type c_schar = i8;
-pub type c_uchar = u8;
-pub type c_short = i16;
-pub type c_ushort = u16;
-pub type c_int = i32;
-pub type c_uint = u32;
-pub type c_long = i64;
-pub type c_ulong = u64;
-pub type c_longlong = i64;
-pub type c_ulonglong = u64;
 pub type intmax_t = i64;
 pub type uintmax_t = u64;
 pub type intptr_t = isize;
 pub type uintptr_t = usize;
-
-pub type c_float = f32;
-pub type c_double = f64;
 
 pub type size_t = usize;
 pub type ptrdiff_t = isize;
@@ -40,6 +21,9 @@ pub const STDIN_FILENO: c_int = 0;
 pub const STDOUT_FILENO: c_int = 1;
 pub const STDERR_FILENO: c_int = 2;
 
+mod error;
+pub use error::*;
+
 cfg_if! {
     if #[cfg(target_arch = "aarch64")] {
         mod aarch64;
@@ -51,5 +35,3 @@ cfg_if! {
         // Unknown target_arch
     }
 }
-
-pub use ffi::c_void;
